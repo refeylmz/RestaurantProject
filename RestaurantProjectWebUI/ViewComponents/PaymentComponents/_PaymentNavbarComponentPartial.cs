858ee4a1246307aced6b19f4fab848bd3e36b7ad
refeylmz <112117584+refeylmz@users.noreply.github.com>
2024-05-26 01:04:30 +0300

    Payment İndex oluşturuldu

---
+++ RestaurantProjectWebUI/ViewComponents/PaymentComponents/_PaymentNavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantProjectWebUI.ViewComponents.PaymentComponents
+{
+    public class _PaymentNavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
